802d53b90274c1c273879f466a8cd6b5e12d0bbe
boodac <180095661+Boodac@users.noreply.github.com>
2024-09-24 14:57:31 -0400

    Refactor and finalize display agnostic tic-tac-toe logic

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,28 @@
 function createGame(obj1, obj2) {
-    const options = { default: ".", x: "x" , o: "o", size: 3}
+    const options = {default: ".", x: "x" , o: "o", size: 3}
     const player1 = {mark: options.x, ...obj1};
     const player2 = {mark: options.o, ...obj2};
-    const board = (function(size = options.size) {
+    let winBool = false;
+    let currPlayer = player1;
+    const board = (function() {
         let state = [];
-        for(let i = 0 ; i < size ; i++) {
+        for(let i = 0 ; i < options.size ; i++) {
             let row = [];
-            for(let j = 0 ; j < size ; j++) {
+            for(let j = 0 ; j < options.size ; j++) {
                 row.push(options.default);
             }
             state.push(row);
-        }
+        };
         return state;
     })();
-    let currPlayer = player1;
-
     const changeTurn = () => { (currPlayer === player1) ? currPlayer = player2 : currPlayer = player1; }
-
-    const makeMark = (x, y) => { // 
-        let row = board[y];
-        (row[x] === ".") ? row[x] = currPlayer.mark : console.log("error attempting to place mark");
-        showBoard();
+    const makeMark = (xCoord, yCoord) => { // 
+        let row = board[yCoord];
+        (row[xCoord] === ".") ? row[xCoord] = currPlayer.mark : console.log("error attempting to place mark");
         checkWin() ? declareWin(currPlayer) : changeTurn();
     }
-
-    const showBoard = () => {
-        board.forEach(element => {
-            console.log(element);
-        });
-    }
-
     const checkWin = () => {
-        let winBool = false;
-        // create diagonals
-        const diags = (function () {
+        const diags = (function () { // create diagonals
             let top = [];
             for(let i = 0 ; i < options.size ; i++) {
                 top.push(board[i][i]);
@@ -44,42 +33,31 @@ function createGame(obj1, obj2) {
             }
             return [ top, bottom ];
         })();
-
-        // transpose the board
-        const cols = board[0].map(
-            (col, i) => board.map(row => row[i])
+        const cols = board[0].map( // transpose the board
+            (column, i) => board.map(row => row[i])
         )
-
-        checkLines = (matrix) => {
+        const checkLines = (matrix) => {
             matrix.forEach(row => {
                 if(winBool) return;
                 if(row[0] === options.default) return;
                 winBool = row.every(value => value === row[0]);
             });
         }
-
-        checkLines(board), checkLines(diags), checkLines(cols);
-
+        checkLines(board); checkLines(diags); checkLines(cols);
         return winBool;
     }
-
     const declareWin = (currPlayer) => {
-        console.log(currPlayer.name + " wins!");
+        console.log("Win detected");
+        currPlayer.win();
         winBool = false;
     }
-
-    return { player1, player2, makeMark, showBoard, checkWin, board };
+    return { player1, player2, makeMark, checkWin, board };
 }
 
 function createPlayer(name) {
     let scoreTotal = 0;
     const win = () => ++scoreTotal;
     const getScore = () => scoreTotal;
-
     return { name, win, getScore };
 }
-
-let John = createPlayer("john");
-let Fred = createPlayer("fred");
-
-const game = createGame(John, Fred);
+const game = createGame(createPlayer("john"), createPlayer("milton"));
